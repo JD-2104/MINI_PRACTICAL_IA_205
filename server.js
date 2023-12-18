2d1a529aa06e6e7f9dbce3529668b5a6eac48537
Joshua Danso <jdanso2104@gmail.com>
2023-12-18 09:23:21 +0000

    initial commit

--- server.js
+++ server.js
@@ -3,4 +3,13 @@ const app = express()
 
 const mongoose = require('mongoose')
 
+mongoose.connect
+
 app.listen(3000, () => console.log('Server has started'))
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+

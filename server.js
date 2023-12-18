b7f5e5db747bdbc0d2cf4533a27409f74e3b01f0
Joshua Danso <jdanso2104@gmail.com>
2023-12-18 09:28:56 +0000

    add

--- server.js
+++ server.js
@@ -13,3 +13,10 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+
+app.use(express.json())
+
+const subscriberRouter = require('./routes/patient')
+app.use('/subscriber', subscriberRouter)
+
+ app.listen(3000, () => console.log('Server has started'))
